a332f895dee1cb63b3a3fdf0adf06c1c863d3989
Michael-Aga <MichaelAgarkov@gmail.com>
2023-12-22 18:09:37 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -53,11 +53,14 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+/*if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+}*/
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
